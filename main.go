1044e713352f263aaec618abc7de1db357a273dd
rizqito <qito25@gmail.com>
2021-08-08 21:41:13 +0800

    aaa

--- main.go
+++ main.go
@@ -6,42 +6,8 @@ import (
 )
 
 func main() {
+	// cara pemanggilan function dalam package lain
+
 	fmt.Println("-----Hello World-------")
 	helper.SayHello("Rizqi")
-
-	fmt.Println("------variabel---------")
-	helper.Variabel()
-
-	fmt.Println("------Try--------------")
-	helper.Try()
-
-	fmt.Println("------Array Slice------")
-	helper.ArraySlices()
-
-	fmt.Println("-------Map-------------")
-	helper.Map()
-
-	fmt.Println("-------Pointer---------")
-	helper.Pointer()
-
-	fmt.Println("-------PrintF----------")
-	helper.CetakF()
-
-	fmt.Println("-------Random----------")
-	helper.Random()
-
-	fmt.Println("-------SQRT------------")
-	helper.Sqrt()
-
-	fmt.Println("-------Structs---------")
-	helper.BelajarStruct()
-
-	fmt.Println("-------Function--------")
-	helper.Fungsi()
-
-	fmt.Println("--------Method---------")
-	helper.BelajarMethod()
-
-	fmt.Println("-------INterface-------")
-	helper.Interface()
 }
