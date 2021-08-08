0b6da9d76dd1d331f0d317a33c71dcf131c64efa
rizqito <qito25@gmail.com>
2021-08-08 12:25:15 +0800

    asdasd

--- latihan/interface.go
+++ latihan/interface.go
@@ -4,7 +4,7 @@ package main
 import "fmt"
 
 // interface berguna untuk method bisa dipake berulang-ulang
-type BangunDatar interface{
+type BangunDatar interface {
 	Luas()
 	Keliling()
 }
@@ -12,63 +12,63 @@ type BangunDatar interface{
 // beberapa objek
 
 // objek persegi
-type Persegi struct{
+type Persegi struct {
 	Sisi float64
 }
 
 // objek persegi panjang
-type PersegiPanjang struct{
+type PersegiPanjang struct {
 	Panjang float64
-	Lebar float64
+	Lebar   float64
 }
 
-type Lingkaran struct{
+type Lingkaran struct {
 	Jari2 float64
 }
 
 // method with receiver param = (biasanya digunakan untuk langsung inject ke struct)
 
 // method persegi
-func(p Persegi) Luas() float64{
+func (p Persegi) Luas() float64 {
 	return p.Sisi * p.Sisi
 }
 
-func(p Persegi) Keliling() float64{
+func (p Persegi) Keliling() float64 {
 	return 4 * p.Sisi
 }
 
 // method persegi panjang
-func(pp PersegiPanjang) Luas() float64{
+func (pp PersegiPanjang) Luas() float64 {
 	return pp.Panjang * pp.Lebar
 }
 
-func(pp PersegiPanjang) Keliling() float64{
+func (pp PersegiPanjang) Keliling() float64 {
 	return 2 * (pp.Panjang + pp.Lebar)
 }
 
 // method lingkaran
-func(l Lingkaran) Luas() float64{
+func (l Lingkaran) Luas() float64 {
 	return 3.14 * (l.Jari2 * l.Jari2)
 }
 
 // function persegi
-func hitungPersegi(p Persegi){
+func hitungPersegi(p Persegi) {
 	fmt.Println("Keliling Persegi : ", p.Keliling())
 	fmt.Println("LUas Persegi : ", p.Luas())
 }
 
 // function persegi panjang
-func hitungPersegiPanjang(pp PersegiPanjang){
+func hitungPersegiPanjang(pp PersegiPanjang) {
 	fmt.Println("Keliling Persegi Panjang : ", pp.Keliling())
 	fmt.Println("LUas Persegi Panjang : ", pp.Luas())
 }
 
 // function lingkaran
-func hitungLingkaran(l Lingkaran){	
+func hitungLingkaran(l Lingkaran) {
 	fmt.Println("LUas Lingkaran : ", l.Luas())
 }
 
-func main(){
+func main() {
 	fmt.Println("----------Persegi---------")
 	hitungPersegi(Persegi{Sisi: 6})
 
